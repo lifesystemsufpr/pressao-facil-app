1ceed6c3e32999674a5037ffc531ea44c7250865
isabellalcvicente <isabellacadastros16@gmail.com>
2026-07-24 17:20:52 -0300

    feat: implementação dos cards de pressão

--- src/app/features/measurements/screens/NovaMedicaoScreen.tsx
+++ src/app/features/measurements/screens/NovaMedicaoScreen.tsx
@@ -1,21 +1,79 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, Pressable, ScrollView } from 'react-native';
 import { RootStackScreenProps } from '../../../shared/types/navigation';
 
 export const NovaMedicaoScreen = ({ navigation }: RootStackScreenProps<'NovaMedicaoModal'>) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Nova Medição</Text>
-      <Button title="Cancelar" onPress={() => navigation.goBack()} />
-      <Button title="Salvar" onPress={() => {
-        // Logica de salvar
-        navigation.goBack();
-      }} />
-    </View>
+    <ScrollView>
+      <View style={styles.measurementsRow}>
+        {/* container da medição das pressoes em linha */}
+        <Pressable style={styles.measurementCard}>
+          <Text>
+            Sistólica (Maior)
+          </Text>
+          <Text>
+            120
+          </Text>
+          <Text>
+            mmHg
+          </Text>
+        </Pressable>
+
+        <Pressable style={styles.measurementCard}>
+          <Text>
+            Diastólica (Menor)
+          </Text>
+          <Text>
+            80
+          </Text>
+          <Text>
+            mmHg
+          </Text>
+        </Pressable>
+        </View>
+
+        
+
+
+        {/* // Logica de salvar
+        // navigation.goBack(); */}
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center', padding: 20, gap: 10 },
-  title: { fontSize: 24, fontWeight: 'bold', marginBottom: 20 },
-});
+    container: {
+        flexGrow: 1,
+        paddingHorizontal: 16,
+        paddingTop: 20,
+        paddingBottom: 110,
+        backgroundColor: '#F8F7FC',
+    },
+
+    measurementsRow: {
+        flexDirection: 'row',
+        gap: 12,
+        marginBottom: 12,
+    },
+
+    measurementCard: {
+        flex: 1,
+        minHeight: 120,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 12,
+        paddingVertical: 16,
+        borderTopWidth: 3,
+        borderTopColor: '#0068C9',
+        borderRadius: 14,
+        backgroundColor: '#FFFFFF',
+        shadowColor: '#000000',
+        shadowOpacity: 0.06,
+        shadowRadius: 6,
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        elevation: 2,
+    }
+    });
